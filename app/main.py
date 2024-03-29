02567f37971d0fb1d0240875f55b70a6b36d88fc
Jinhong Zhao <asg2rg@virginia.edu>
2024-03-29 18:38:56 +0000

    In class activity

--- app/main.py
+++ app/main.py
@@ -5,10 +5,18 @@ from typing import Optional
 from pydantic import BaseModel
 import json
 import requests
+import os
+import MySQLdb
+from fastapi.staticfiles import StaticFiles
 # import boto3
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
+DBHOST = os.environ.get('DBHOST')
+DBUSER = os.environ.get('DBUSER')
+DBPASS = os.environ.get('DBPASS')
+DB = "asg2rg"  # replace with your UVA computing ID / database name
 # The URL for this API has a /docs endpoint that lets you see and test
 # your various endpoints/methods.
 
